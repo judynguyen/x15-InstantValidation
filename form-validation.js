674ee41dd8fdf579cc1177ef175f7d0f11f0dd5b
Jude <jnguyen41@mail.csuchico.edu>
2016-10-12 17:08:43 -0700

    new updates for advanced form validation

--- form-validation.js
+++ form-validation.js
@@ -3,10 +3,11 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
   var userLength = userEntered.length;
-  var nope = "password"
+  var passLength = passEntered.length;
 
   console.log("Username length is " +  userLength);
   console.log("Password is " + passEntered);
+  console.log("Password length is " + passLength);
 
   if(userLength>=6)
   {
@@ -26,13 +27,21 @@ function validate(){
     document.getElementById("usernameGroup").classList.add("has-error");
   }
 
-  if(passEntered == nope)
+
+  if(passEntered.toLowerCase() == "password" || passEntered > 20)
   {
     document.getElementById("passwordError").innerHTML="Bad password.";
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
     //Turn the password items red
     document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else if (passLength < 6) {
+  document.getElementById("passwordError").innerHTML="Password must be greater than 6 characters.";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordError").classList.add("shown-message");
+  //Turn the password items red
+  document.getElementById("passwordGroup").classList.add("has-error");
   }
   else
   {
@@ -43,4 +52,29 @@ function validate(){
     document.getElementById("passwordGroup").classList.remove("has-error");
     document.getElementById("passwordGroup").classList.add("has-success");
   }
+
+  if(passEntered==userEntered)
+  {
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+
+  for(var i=0; i<=userLength; i++)
+  {
+      var ch;
+      ch = userEntered.charAt(i);
+      console.log(ch);
+      if(ch == " ")
+      {
+        document.getElementById("usernameError").innerHTML="Bad username.";
+        document.getElementById("usernameError").classList.remove("hidden-message");
+        document.getElementById("usernameError").classList.add("shown-message");
+        //Turn the username items red
+        document.getElementById("usernameGroup").classList.add("has-error");
+      }
+  }
+
 }
